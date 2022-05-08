4066f2e2e070e62c0411b677227a9f1aaf4b567d
JANINDU MALEESHA <janindumaleesha99@gmail.com>
2022-05-08 20:47:07 +0530

    Create search

--- src/main.js
+++ src/main.js
@@ -8,12 +8,23 @@ import 'buefy/dist/buefy.css'
 
 import 'mdb-vue-ui-kit/css/mdb.min.css';
 
+
+
+
 Vue.use(Buefy)
+import { ToastProgrammatic as Toast } from 'buefy'
 
 Vue.config.productionTip = false;
+
+Vue.filter('to-uppercase',function(value){
+  return value.toUpperCase();
+})
+
+Toast.open('Toasty!')
 
 new Vue({
   router,
   store,
   render: (h) => h(App),
 }).$mount("#app");
+
